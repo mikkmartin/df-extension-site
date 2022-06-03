5d7779f9740bf04f6812044e1189317627bf0fc0
Mikk Martin <mikk.martin@gmail.com>
2022-06-03 14:34:03 +0300

    some cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components'
-import Image from 'next/image'
 import { Layout } from 'components/Layout'
 import { motion } from 'framer-motion'
 
 export default function Home() {
-  const slug = 'photography-blog-og'
   return (
     <Layout>
       <Container>
