ce15061fa75101cfca785aa95e93db7b4132bc5f
Mikk Martin <mikk.martin@gmail.com>
2022-06-06 12:38:04 +0300

    more template content

--- data/defaultTemplates.ts
+++ data/defaultTemplates.ts
@@ -12,8 +12,16 @@ export const defaultTemplates = [
     name: 'Levila.ee',
     description: 'Loe. Kuula. Vaata. Levila artiklitest story piltide looja.',
     images: [
-      { id: 'd54Vs', src: 'story-levila-1.png', url: 'https://www.levila.ee/tekstid/kirjutan-teile-ukrainast' },
-      { id: '9usOA', src: 'story-levila-2.png', url: 'https://www.levila.ee/tekstid/levila-avas-veebipoe' },
+      {
+        id: 'd54Vs',
+        src: 'story-levila-1.png',
+        url: 'https://www.levila.ee/tekstid/kirjutan-teile-ukrainast',
+      },
+      {
+        id: '9usOA',
+        src: 'story-levila-2.png',
+        url: 'https://www.levila.ee/tekstid/levila-avas-veebipoe',
+      },
       { id: 'g6a3S', src: 'story-levila-3.png', url: 'https://www.levila.ee/video/mereprugila' },
     ],
   },
@@ -21,18 +29,42 @@ export const defaultTemplates = [
     name: 'Edasi.ee',
     description: '',
     images: [
-      { id: '3PwYR', src: 'edasi-1.png', url: '' },
-      { id: 'EVYXI', src: 'edasi-2.png', url: '' },
-      { id: '1KstX', src: 'edasi-3.png', url: '' },
+      {
+        id: '3PwYR',
+        src: 'edasi-1.png',
+        url: 'https://edasi.org/101955/valner-valme-raamatusoovitused-aafrikat-kanget-luulet-punki-kuubat-halba-ilma/',
+      },
+      {
+        id: 'EVYXI',
+        src: 'edasi-2.png',
+        url: 'https://edasi.org/132074/mart-kangro-kujundilugemise-oskus-on-uhiskonnas-halvas-seisus/',
+      },
+      {
+        id: '1KstX',
+        src: 'edasi-3.png',
+        url: 'https://edasi.org/132006/teatriarvustus-kaja-kann-koreograafid-kergitavad-kehalist-leivatainast/',
+      },
     ],
   },
   {
     name: 'Muurileht.ee',
     description: '',
     images: [
-      { id: 'IC2cN', src: 'muurileht-1.png', url: '' },
-      { id: 'lvhZU', src: 'muurileht-2.png', url: '' },
-      { id: '0Rigk', src: 'muurileht-3.png', url: '' },
+      {
+        id: 'IC2cN',
+        src: 'muurileht-1.png',
+        url: 'https://www.muurileht.ee/juhtkiri-paike-tousis-touseb-ja-jaab-tousma-idast/',
+      },
+      {
+        id: 'lvhZU',
+        src: 'muurileht-2.png',
+        url: 'https://www.muurileht.ee/kui-tribal-on-uus-traditional-ehk-eesti-tattoo-skeenest-aastal-2022/',
+      },
+      {
+        id: '0Rigk',
+        src: 'muurileht-3.png',
+        url: 'https://www.muurileht.ee/otsides-graafilise-disaini-tegemise-viise-ekraani-taga-ja-sellest-eemal/',
+      },
     ],
   },
 ]
