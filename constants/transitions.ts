a9434153817e5c70b6fa655a82b51c54a940c891
Mikk Martin <mikk.martin@gmail.com>
2022-06-16 15:01:17 +0300

    Squashed commit of the following:
    commit baf0190a0d4435b0e22c57a1ea9c3d6753566383
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Thu Jun 16 12:15:31 2022 +0300
    
        scraper wip
    
    commit a4406949f7a9d2e904da8c50e3ac8f9431e4ead8
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Wed Jun 15 17:02:32 2022 +0300
    
        overlay adjustment
    
    commit 794162b9ed725750591fb67fbf34ffd9ad1ed1f4
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Wed Jun 15 16:49:16 2022 +0300
    
        mobile input
    
    commit 24bd645a7280c430d9aab82204b95ac42ca91c1c
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Wed Jun 15 15:56:38 2022 +0300
    
        desktop adjustments
    
    commit a24b2082848d1c41756f47b2b356f56ca55ca09f
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Wed Jun 15 15:53:17 2022 +0300
    
        desktop modal
    
    commit 710b8d239654df89d34a0d78cd2dc84710c67988
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Wed Jun 15 14:57:49 2022 +0300
    
        some cleanup
    
    commit 462e6cccb9b795cdef00c3082ba2fcd76087eae5
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Wed Jun 15 13:59:55 2022 +0300
    
        layout adjustments
    
    commit c114ca482ab7ff588b3054c59b28cbc94cb4ea09
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Wed Jun 15 12:43:38 2022 +0300
    
        decent desktop transition
    
    commit b098476ff0f2caba3899638a859b83487975fd6e
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Tue Jun 14 19:58:04 2022 +0300
    
        pretty decent
    
    commit ae2ecd08466eae3140fb6bcfe349acdb076bd9a5
    Author: Mikk Martin <mikk.martin@gmail.com>
    Date:   Tue Jun 14 17:48:13 2022 +0300
    
        responsive wip

---
+++ constants/transitions.ts
@@ -0,0 +1,5 @@
+export const slow = { type: 'spring', stiffness: 200, damping: 120, mass: 1 }
+export const bouncy = { type: 'spring', stiffness: 650, damping: 30, mass: 1 }
+export const smooth = { type: 'spring', stiffness: 500, damping: 40, mass: 1 }
+export const fast = { type: 'spring', stiffness: 2000, damping: 120, mass: 1 }
+export const snappy = { type: 'spring', stiffness: 2500, damping: 40, mass: 0.01 }
